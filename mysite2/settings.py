1236b9177bbad1b644313121b1b9e8d188e11541
hakevin <37036561+hakevin@users.noreply.github.com>
2018-04-01 18:27:19 -0700

    Update settings.py
    Undo last commit. Not sure how to change folder names.

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangoblog.urls'
+ROOT_URLCONF = 'mysite2.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'djangoblog.wsgi.application'
+WSGI_APPLICATION = 'mysite2.wsgi.application'
 
 
 # Database
